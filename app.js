7b1e3e9e05dc12c4cad0d2ffcdb0388ba90345cc
charliehyin <charliehyin@gmail.com>
2024-06-29 03:37:24 -0500

    Changing port to 8080

--- app.js
+++ app.js
@@ -98,7 +98,7 @@ app.get('/health', (req, res) => {
     res.send('OK');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Health check server running on port ${PORT}`);
 });

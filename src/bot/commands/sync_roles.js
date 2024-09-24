cd6b798f1faa7149ac00fd41307d87c0142ddd40
Charliehyin <charliehyin@gmail.com>
2024-09-24 09:58:11 -0500

    feat: rewrite sync roles command to be more efficient with the discord api, and defer/edit reply

--- src/bot/commands/sync_roles.js
+++ src/bot/commands/sync_roles.js
@@ -4,24 +4,21 @@ const { get_guild_info } = require('../utils/get_guild_info');
 const { check_ironman_hotm4 } = require('../utils/check_ironman_hotm4');
 const { ims_guild_id, imc_guild_id, ima_guild_id, ims_guild_role, imc_guild_role, ima_guild_role, king_role, god_role, divine_role, lfp_plus_role, verified_role } = require('../constants');
 
-const add_rank_role = async (interaction, rank, roles) => {
+const add_rank_role = async (rank, roles) => {
     if (rank === 'King') {
-        await interaction.member.roles.add(king_role);
         console.log('    King role added');
         roles.push(king_role);
     } else if (rank === 'God') {
-        await interaction.member.roles.add(god_role);
         console.log('    God role added');
         roles.push(god_role)
     } else if (rank === 'Divine') {
-        await interaction.member.roles.add(divine_role);
         console.log('    Divine role added');
         roles.push(divine_role)
     }
     return roles;
 }
 
-const sync_guild_info = async (interaction, uuid) => {
+const sync_guild_info = async (uuid) => {
     const [guild_id, rank] = await get_guild_info(uuid);
     let roles = [verified_role];
 
@@ -33,31 +30,28 @@ const sync_guild_info = async (interaction, uuid) => {
     if (guild_id === ims_guild_id) {
         console.log(`    ${uuid} is in IMS guild`);
 
-        interaction.member.roles.add(ims_guild_role);
         console.log('    IMS role added');
         roles.push(ims_guild_role);
 
-        roles = add_rank_role(interaction, rank, roles);
+        roles = add_rank_role(rank, roles);
     } 
     // Check if user is in IMC guild
     if (guild_id === imc_guild_id) {
         console.log(`    ${uuid} is in IMC guild`);
 
-        interaction.member.roles.add(imc_guild_role);
         console.log('    IMC role added');
         roles.push(imc_guild_role);
 
-        roles = add_rank_role(interaction, rank, roles);
+        roles = add_rank_role(rank, roles);
     } 
     // Check if user is in IMA guild
     if (guild_id === ima_guild_id) {
         console.log(`    ${uuid} is in IMA guild`);
 
-        interaction.member.roles.add(ima_guild_role);
         console.log('    IMA role added');
         roles.push(ima_guild_role);
 
-        roles = add_rank_role(interaction, rank, roles);
+        roles = add_rank_role(rank, roles);
     }
     return roles;
 }
@@ -68,6 +62,7 @@ const sync_roles_command = new SlashCommandBuilder()
 
 const sync_roles_interaction = async (interaction, db) => {
     console.log('Syncing roles')
+    interaction.deferReply();
     // Sync guild info
     try {
         const discord_id = interaction.member.id;
@@ -84,29 +79,64 @@ const sync_roles_interaction = async (interaction, db) => {
 
         console.log(`    uuid: ${uuid}, discord_id: ${discord_id}`)
 
-        await interaction.member.roles.remove([king_role, god_role, divine_role, ims_guild_role, imc_guild_role, ima_guild_role, lfp_plus_role]);
-        console.log('    Old roles removed');
-
-        interaction.member.roles.add(verified_role);
-
-        let roles = await sync_guild_info(interaction, uuid);
+        // Get roles the user should have
+        let roles = await sync_guild_info(uuid);
         console.log('    Guild roles synced')
 
         // Check if user is ironman in HOTM4
         const ironman = await check_ironman_hotm4(uuid);
 
         if (ironman) {
-            interaction.member.roles.add(lfp_plus_role);
-            console.log('    lfp plus role added');
+            console.log('    lfp plus role');
             roles.push(lfp_plus_role);
         }
 
+        // Fetch the member's current roles
+        const memberRoles = interaction.member.roles.cache;
+        const rolesToSync = [ims_guild_role, imc_guild_role, ima_guild_role, king_role, god_role, divine_role, lfp_plus_role, verified_role]
+
+        // Remove roles that the member shouldn't have
+        let rolesToRemove = [];
+        for (const role of memberRoles.keys()) {
+            if (!roles.includes(role) && rolesToSync.includes(role)) {
+                rolesToRemove.push(role);
+                console.log(`    Removing role: ${role}`);
+            }
+        }
+
+        // Define the roles that should be added
+        const rolesToAdd = [];
+        for (const role of roles) {
+            if (!memberRoles.has(role)) {
+                rolesToAdd.push(role);
+                console.log(`    Adding role: ${role}`);
+            }
+        }
+
+        await Promise.all([
+            ...rolesToAdd.map(role => interaction.member.roles.add(role)),
+            ...rolesToRemove.map(role => interaction.member.roles.remove(role))
+        ]);
+
+        let reply = '';
+        if (rolesToAdd.length > 0) {
+            console.log('    Adding roles: ' + rolesToAdd.map((role) => '<@&' + role + '>').join(', '));
+            reply += 'Adding: ' + rolesToAdd.map((role) => '<@&' + role + '>').join(', ') + '\n';
+        }
+        if (rolesToRemove.length > 0) {
+            console.log('    Removing roles: ' + rolesToRemove.map((role) => '<@&' + role + '>').join(', '));
+            reply += 'Removing: ' + rolesToRemove.map((role) => '<@&' + role + '>').join(', ') + '\n';
+        }
+        if (reply === '') {
+            reply = 'No roles to add or remove';
+        }
+
         console.log('    Done syncing roles');
-        await interaction.reply('Roles synced: ' + roles.map((role) => '<@&' + role + '>').join(', '));
+        await interaction.editReply(reply);
 
     } catch (error) {
         console.error('Error syncing guild info:', error);
-        await interaction.reply(`There was an error while trying to sync your roles: ${error.message}`);
+        await interaction.editReply(`There was an error while trying to sync your roles: ${error.message}`);
     }
 }
 
